a356e15c60558ff28fe297b02e7f62045be10072
Cherita Ousley <cheritaousley@Cheritas-MacBook-Air.local>
2017-12-19 17:22:12 -0600

    merging updates

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,34 +8,28 @@ console.log("MADE IT TO ROUTES.JS");
 
 module.exports = function (app) {
     
-    // app.get('/API/getUser', function (req, res) {
-    // UserController.getAll(req, res); })
+    app.get('/API/getUser', function (req, res) {
+    UserController.getAll(req, res); })
 
-    // app.get('/API/getTeam', function (req, res) {
-    //     TeamController.getAll(req, res);
-    // })
+    app.get('/API/getTeam', function (req, res) {
+        TeamController.getAll(req, res);
+    })
 
-<<<<<<< HEAD
     app.get('/API/getMessage', function (req, res) {
         ChatController.getAll(req, res);
     })
-=======
-    // app.get('/API/getMessage', function (req, res) {
-    //     ChatController.getAll(req, res);
-    // })
->>>>>>> upstream/master
 
-    // app.get('/API/getChannel', function (req, res) {
-    //     ChannelController.find(req, res);
-    // })
+    app.get('/API/getChannel', function (req, res) {
+        ChannelController.find(req, res);
+    })
     
-    // app.post('/API/createUser', function (req, res) {
-    //     UserController.createUser(req, res);
-    // })
+    app.post('/API/createUser', function (req, res) {
+        UserController.createUser(req, res);
+    })
 
-    // app.post('/API/createTeam', function (req, res) {
-    //     TeamController.createTeam(req, res);
-    // })
+    app.post('/API/createTeam', function (req, res) {
+        TeamController.createTeam(req, res);
+    })
     app.post('/API/createChannel', function (req, res) {
         ChannelController.createChannel(req, res);
     })
